5d77794a19d60991c70f61d8e452196a8071917e
varadharajan1711 <varadharajansekar@gmail.com>
2019-11-20 10:26:58 +0530

    uploaded files
    a good running sample for Bob, Matching brackets, TwoFer

---
+++ Bob.cs
@@ -0,0 +1,38 @@
+﻿namespace CSharpPractice11Nov19
+{
+	public class Bob
+	{
+		/// <summary>
+		/// Bob[Flow Control]
+		/// Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+		/// </summary>
+		/// <param name="communication"></param>
+		/// <returns></returns>
+		#region Publics
+		public string Conversation(string communication)
+		{
+			if(communication.Length == 0)
+			{
+				return "Fine. Be that way!";
+			}
+
+			if(communication.Contains("?") && !communication.Contains("!"))
+			{
+				return "Sure.";
+			}
+
+			if(!communication.Contains("?") && communication.Contains("!"))
+			{
+				return "Whoa, Chill out!";
+			}
+
+			if(communication.Contains("?") && communication.Contains("!"))
+			{
+				return "Calm down, I know what I'm doing!";
+			}
+
+			return "Whatever.";
+		}
+		#endregion
+	}
+}
---
+++ BobUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using CSharpPractice11Nov19;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpPractice11Nov19UnitTest
+{
+	[TestClass]
+	public class BobUnitTest
+	{
+		#region Fields
+		private string strCommunication;
+		#endregion
+
+		#region Tests
+		[TestMethod]
+		public void TestQuestion() //?
+		{
+			// Arrange
+			Bob b = new Bob();
+			strCommunication = "Can you do this job for me?";
+
+			// Act
+			string strResult = b.Conversation(strCommunication);
+
+			// Assert
+			Assert.AreEqual("Sure.", strResult);
+		}
+
+		[TestMethod]
+		public void TestYell() // !
+		{
+			// Arrange
+			Bob b = new Bob();
+			strCommunication = "Just, Shut up!";
+
+			// Act
+			string strResult = b.Conversation(strCommunication);
+
+			// Assert
+			Assert.AreEqual("Whoa, Chill out!", strResult);
+		}
+
+		[TestMethod]
+		public void TestYellnQuestion() // !?
+		{
+			// Arrange
+			Bob b = new Bob();
+			strCommunication = "Oh God!, What the hell are you doing?";
+
+			// Act
+			string strResult = b.Conversation(strCommunication);
+
+			// Assert
+			Assert.AreEqual("Calm down, I know what I'm doing!", strResult);
+		}
+
+		[TestMethod]
+		public void TestNothing() // null
+		{
+			// Arrange
+			Bob b = new Bob();
+			strCommunication = "";
+
+			// Act
+			string strResult = b.Conversation(strCommunication);
+
+			// Assert
+			Assert.AreEqual("Fine. Be that way!", strResult);
+		}
+
+		[TestMethod]
+		public void TestAnythingElse() // default
+		{
+			// Arrange
+			Bob b = new Bob();
+			strCommunication = "Sorry";
+
+			// Act
+			string strResult = b.Conversation(strCommunication);
+
+			// Assert
+			Assert.AreEqual("Whatever.", strResult);
+		}
+		#endregion
+	}
+}
